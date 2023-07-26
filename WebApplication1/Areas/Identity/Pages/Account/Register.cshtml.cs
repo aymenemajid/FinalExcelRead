055234ef0301e5328eb80c4375f84215a2722c21
Soufiane4906 <anibasoufiane2001@gmail.com>
2023-07-26 19:39:14 +0100

    last2

--- WebApplication1/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ WebApplication1/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -141,7 +141,7 @@ namespace WebApplication1.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
-                    await _userManager.AddToRoleAsync(user, Enums.Roles.Basic.ToString());
+                    await _userManager.AddToRoleAsync(user, Enums.Roles.Admin.ToString());
 
 /*                    var userId = await _userManager.GetUserIdAsync(user);
 */                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
